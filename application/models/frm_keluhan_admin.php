bf99c241c21a688f113efb0ca0a6c5495b034bae
Feralfi <olivera7.ov@gmail.com>
2015-06-04 07:48:33 +0700

    ...
    keluhan

---
+++ application/models/frm_keluhan_admin.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined(‘BASEPATH’)) exit(‘No direct script access allowed’);
+class Dasbor extends CI_Controller {
+public function index() {
+$data=array(‘title’=>’Halaman Administrator – Java Web Media’,
+‘isi’ =>’admin/dasbor/dasbor_view’
+);
+$this->load->view(‘admin/layout/wrapper’,$data);
+}
+}
