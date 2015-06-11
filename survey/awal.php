4993dc73f3bfa0573250637a010bffaed00a700a
Dodo Zulkarnain <dodozulkarnain29@gmail.com>
2015-06-11 12:56:38 +0700

    Survey
    Tampilan

---
+++ survey/awal.php
@@ -0,0 +1,117 @@
+<html>
+<head><title>UKM Survey</title>
+<head>
+
+<body background="../views/gambar/images.jpg"><center>
+<table align="center">
+	<tr>
+		<td colspan="3" align="center">
+			<h1>Survey E-commerce</h1></br>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="3"><a href="views/daftar"><input type="button" value="Daftar"></a></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td align="center"><h2>Daftar UKM</h2></td>
+		<td></td>
+	</tr>
+	<tr align="center">
+		<td>
+			<table>
+				<tr>
+					<td align="center" colspan="3">
+					Gambar
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<input type="button" value="Lihat">|<input type="button" value="Ubah">|<input type="button" value="Hapus">
+					</td>
+				</tr>
+			</table>
+		</td>
+		<td>
+			<table>
+				<tr>
+					<td align="center" colspan="3">
+					Gambar
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<input type="button" value="Lihat">|<input type="button" value="Ubah">|<input type="button" value="Hapus">
+					</td>
+				</tr>
+			</table>
+		</td>
+		<td>
+			<table>
+				<tr>
+					<td align="center" colspan="3">
+					Gambar
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<input type="button" value="Lihat">|<input type="button" value="Ubah">|<input type="button" value="Hapus">
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr align="center">
+		<td>
+			<table>
+				<tr>
+					<td align="center" colspan="3">
+					Gambar
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<input type="button" value="Lihat">|<input type="button" value="Ubah">|<input type="button" value="Hapus">
+					</td>
+				</tr>
+			</table>
+		</td>
+		<td>
+			<table>
+				<tr>
+					<td align="center" colspan="3">
+					Gambar
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<input type="button" value="Lihat">|<input type="button" value="Ubah">|<input type="button" value="Hapus">
+					</td>
+				</tr>
+			</table>
+		</td>
+		<td>
+			<table>
+				<tr>
+					<td align="center" colspan="3">
+					Gambar
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<input type="button" value="Lihat">|<input type="button" value="Ubah">|<input type="button" value="Hapus">
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="3"></br></br></br></br></br></td>
+	</tr>
+	<tr>
+		<td colspan="3" align="center">By: Tim Survey E-commerce & Support by:Pak Deddy :)</td>
+	</tr>
+	</table>
+</center>
+<body>
+<html>
---
+++ survey/daftar.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<title>Pendaftaran UKM Survey</title>
+</head>
+<body><center>
+	<table>
+		<tr>
+			<td colspan="3" align="center"><h1>Silahkan mengisi Daftar Survey UKM</h1></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" placeholder="Nama UKM"></td>
+		</tr>
+		<tr>
+			<td>Milik</td>
+			<td>:</td>
+			<td><input type="text" name="milik" placeholder="Nama Pemilik"></td>
+		</tr>
+		<tr>
+			<td>Lokasi</td>
+			<td>:</td>
+			<td><input type="text" name="lokasi" placeholder="Lokasi UKM"></td>
+		</tr>
+		<tr>
+			<td>No telepon</td>
+			<td>:</td>
+			<td><input type="telephone" name="telepon" placeholder="Telepon"></td>
+		</tr>
+		<tr>
+			<td>Tahun Berdiri</td>
+			<td>:</td>
+			<td><input type="text" name="nama" placeholder="Tahun"></td>
+		</tr>
+		<tr>
+			<td>email</td>
+			<td>:</td>
+			<td><input type="email" name="email" placeholder="email"></td>
+		</tr>
+		<tr>
+			<td>Deskripsi</td>
+			<td>:</td>
+			<td><input type="text" name="nama" placeholder="Penjelasan UKM"></td>
+		</tr>
+		<tr>
+			<td>Gambar</td>
+			<td>:</td>
+			<td><input type="text" name="nama" placeholder="Nama UKM"></td>
+		</tr>
+	</table>
+	</center>
+</body>
+</html>
