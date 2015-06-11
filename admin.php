72a1bb567fb846e90d2bdeafa3d400ad65383e1a
amaliaagung <amaliagung@gmail.com>
2015-06-11 11:00:22 +0800

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,53 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+	function __construct() {
+		parent::__construct();
+	}
+	
+	public function index() {
+		if ($this->session->userdata('admin_valid') == FALSE && $this->session->userdata('admin_id') == "") {
+			redirect("index.php/admin/login");
+		}
+		
+		$a['page']	= "d_amain";
+		
+		$this->load->view('admin/aaa', $a);
+	}
+	
+	public function login() {
+		$this->load->view('admin/login');
+	}
+	
+	public function do_login() {
+		$u 		= $this->security->xss_clean($this->input->post('u'));
+		$ta 	= $this->security->xss_clean($this->input->post('ta'));
+        $p 		= md5($this->security->xss_clean($this->input->post('p')));
+         
+		$q_cek	= $this->db->query("SELECT * FROM t_admin WHERE username = '".$u."' AND password = '".$p."'");
+		$j_cek	= $q_cek->num_rows();
+		$d_cek	= $q_cek->row();
+		//echo $this->db->last_query();
+		
+        if($j_cek == 1) {
+            $data = array(
+                    'admin_id' => $d_cek->id,
+                    'admin_user' => $d_cek->username,
+                    'admin_nama' => $d_cek->nama,
+                    'admin_ta' => $ta,
+                    'admin_level' => $d_cek->level,
+					'admin_valid' => true
+                    );
+            $this->session->set_userdata($data);
+            redirect('index.php/admin');
+        } else {	
+			$this->session->set_flashdata("k", "<div id=\"alert\" class=\"alert alert-error\">username or password is not valid</div>");
+			redirect('index.php/admin/login');
+		}
+	}
+	
+	public function logout(){
+        $this->session->sess_destroy();
+		redirect('index.php/admin/login');
+    }
+}
