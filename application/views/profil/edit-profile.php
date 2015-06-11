73660c201decd6370944b060ef3b659555bb411f
AuliyaAzam <auliyaazambakhtiar@gmail.com>
2015-06-11 17:50:05 +0700

    Survey
    Edit Profile

---
+++ application/views/profil/edit-profile.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US"><head><!-- Created by Artisteer v4.0.0.58475 -->
+    <meta charset="utf-8">
+    <title>Edit Profil</title>
+    <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width">
+
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href=" <?php base_url(); ?> file/css/style.css" media="screen">
+    <!--[if lte IE 7]><link rel="stylesheet" href="  <?php base_url(); ?> file/css/style.ie7.css" media="screen" /><![endif]-->
+    <link rel="stylesheet" href="../style.responsive.css" media="all">
+
+
+    <script src=" <?php base_url(); ?> file/js/jquery.js"></script>
+    <script src=" <?php base_url(); ?> file/js/script.js"></script>
+    <script src=" <?php base_url(); ?> file/js/script.responsive.js"></script>
+
+
+</head>
+<body>
+<div id="art-main">
+<header class="art-header clearfix">
+
+
+    <div class="art-shapes">
+<h1 class="art-headline" data-left="67.72%">
+    <a href=" <?php base_url(); ?> #">Call Us Today</a>
+</h1>
+<h2 class="art-slogan" data-left="77.15%">089-800-31-888</h2>
+
+
+            </div>
+
+<nav class="art-nav clearfix">
+    <ul class="art-hmenu"><li><a href=" <?php base_url(); ?> ../home.html">Home</a><ul><li><a href=" <?php base_url(); ?> ../home/register.html">Register</a></li><li><a href=" <?php base_url(); ?> ../home/sign-in.html">Sign In</a></li></ul></li><li><a href=" <?php base_url(); ?> ../profil.html" class="active">Profil</a><ul class="active"><li><a href=" <?php base_url(); ?> ../profil/edit-profile.html" class="active">Edit Profile</a></li></ul></li><li><a href=" <?php base_url(); ?> ../contact-us.html">Contact Us</a></li></ul> 
+    </nav>
+
+                    
+</header>
+<div class="art-sheet clearfix">
+            <div class="art-layout-wrapper clearfix">
+                <div class="art-content-layout">
+                    <div class="art-content-layout-row">
+                        <div class="art-layout-cell art-content clearfix"><article class="art-post art-article">
+                                <h2 class="art-postheader">Edit Profil</h2>
+                                                
+                <div class="art-postcontent art-postcontent-0 clearfix"><div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell" style="width: 33%" >
+        <p><p><p><p><p><p><table class="table-list" width="650" border"0"
+        
+        <p><p><p><p><p> <form id="form1" name="form1" method="post" action="">
+        <p><label for="nama">Nama</label> <input value="" type="text" name="nama" id="nama" /></p>
+        <p><label for="lokasi">Lokasi</label> <input value="" type="text" name="lokasi" id="lokasi" /></p>
+        <p><label for="milik">Milik</label> <input value="" type="text" name="milik" id="milik" /></p>
+        <p><label for="telepon">Telepon</label> <input value="" type="text" name="telepon" id="telepon" /></p>
+        <p><label for="deskripsi">Deskripsi</label> <input value="" name="Deskripsi" type="text" id="deskripsi" /></p>
+        <p><label for="fupload">Gambar</label> <input type="file" size="20" maxlength="10" name="fupload" id="fupload">
+        <form id="form1" name="form1" method="post" action="">
+          
+        <p><label></label>  <input type="submit" name="save" id="save" value="Change" />
+      
+        </form>
+        <p>&nbsp;</p></p></p></p></p></p></p></p></p></p></p></p>
+    </div><div class="art-layout-cell" style="width: 34%" >
+        <p><br /></p>
+    </div><div class="art-layout-cell" style="width: 33%" >
+        <p><br /></p>
+    </div>
+    </div>
+</div>
+</div>
+</article></div>
+                    </div>
+                </div>
+            </div><footer class="art-footer clearfix">
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 34%">
+        <p style="float: left;"><a href="#"><img width="30" height="30" alt="" src=" <?php base_url(); ?> file/images/rss_32.png"></a>&nbsp;&nbsp;<a href=" <?php base_url(); ?> #"><img width="32" height="32" alt="" src=" <?php base_url(); ?> file/images/facebook_32.png" class=""></a>&nbsp;&nbsp;<a href=" <?php base_url(); ?> #"><img width="32" height="32" alt="" src=" <?php base_url(); ?> file/images/twitter_32.png" class=""></a></p>
+    </div><div class="art-layout-cell layout-item-0" style="width: 31%">
+        <p style="float: left;">Copyright TI 3A © 2015-2016. All Rights Reserved.</p>
+    </div><div class="art-layout-cell layout-item-0" style="width: 35%">
+        <p style="float: right;"><a href=" <?php base_url(); ?> #">About Us</a>&nbsp;&nbsp; &nbsp;&nbsp;<a href=" <?php base_url(); ?> #">Contact Us</a>&nbsp;&nbsp; &nbsp;</p>
+    </div>
+    </div>
+</div>
+
+</footer>
+
+    </div>
+    <p class="art-page-footer">
+        <span id="art-footnote-links"><a href=" <?php base_url(); ?> http://www.artisteer.com/" target="_blank">Web Template</a> created with Artisteer.</span>
+    </p>
+</div>
+
+
+</body></html>
