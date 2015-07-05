a7dfb08d0c2745d9117ad34a0d27409b1c0ecb11
umainafara <umainafara@yahoo.com>
2015-07-05 13:25:05 +0700

    profilukm

---
+++ Web Content/profilukm.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action=""><label><label>
+<input name="UKM SUKASUKA" type="text" id="UKM SUKASUKA" value="UKM SUKA SUKA" size="18" />
+</label> 
+  <label>
+<input name="kategori" type="text" id="kategori" value="KATEGORI" size="12" />
+</label>
+<label>
+<input name="cari" type="text" id="cari" value="Cari produk/toko" size="30" />
+</label>
+<img src="file:///F|/unduhan (1).jpg" width="29" height="20" alt="search" />
+<label>
+<input name="bantuan" type="text" id="bantuan" value="Bantuan" size="10" />
+</label>
+<label>
+<input name="daftar" type="text" id="daftar" value="Daftar" size="10" />
+</label>
+<label>
+<input name="masuk" type="text" id="masuk" value="Masuk" size="10" />
+</label>
+<table cellspacing="5" cellpadding="5">
+  <tr>
+    <th width="138" rowspan="3" scope="row"><img src="" alt="" name="imagetoko" width="130" height="150" id="imagetoko2" /></th>
+    <td width="239"><label>
+      <input name="namatoko" type="text" id="namatoko" value="Nama TOKO" />
+    </label></td>
+  </tr>
+  <tr>
+    <td><label>
+      <input name="infotoko" type="text" id="infotoko" value="Biodata TOKO" />
+    </label></td>
+  </tr>
+  In
+  <tr>
+    <td><label>&quot;Semua barang ini adalah handmade&quot;</label></td>
+  </tr>
+</table>
+<p>
+  <label>
+  <input name="produk" type="text" id="produk" value="Produk" size="15" />
+  </label>
+  <label>
+  <input name="diskusi_produk" type="text" id="diskusi_produk" value="Diskusi Produk" size="20" />
+  </label>
+  <label>
+  <input name="review" type="text" id="review" value="Review" size="20" />
+  </label>
+  <label>
+  <input name="info-toko" type="text" id="info-toko" value="Informasi Toko" size="20" />
+  </label>
+</p>
+<p>
+  <label>
+  <input name="cari-produk" type="text" id="cari-produk" value="Cari Produk" />
+  </label>
+  <img src="file:///F|/unduhan (1).jpg" width="29" height="20" alt="search" />	
+  <label></label>
+Urutkan 
+  <label>:
+  <select name="urutkan" id="urutkan">
+    <option>Posisi</option>
+  </select>
+  </label>
+  :	</p>
+<table cellspacing="5" cellpadding="5">
+  <tr>
+    <td width="165">Catatan</td>
+  </tr>
+  <tr>
+    <td><label>
+      <input name="pengem-produk" type="text" id="pengem-produk" value="Kebijakan Pengembalian" size="25" />
+    </label></td>
+  </tr>
+</table>
+<table cellspacing="5" cellpadding="5">
+  <tr>
+    <td width="165">Etalase</td>
+  </tr>
+  <tr>
+    <td><label>
+      <input name="pro-terjual" type="text" id="pro-terjual" value="Produk Terjual" />
+    </label></td>
+  </tr>
+  <tr>
+    <td><label>
+      <input name="semua-etalase" type="text" id="semua-etalase" value="Semua Etalase" />
+    </label></td>
+  </tr>
+  <tr>
+    <td><label>
+      <input name="produk-jual" type="text" id="produk-jual" value="Produk Dijual" />
+    </label></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</form>
+</body>
+</html>
